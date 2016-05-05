a4fdb3c143ea8c5fc9f75bd223b91a644c932b45
zz7 <iltvxqsj@163.com>
2016-05-05 22:13:54 +0800

    add launcher

--- app/src/main/java/com/example/zz7/hiragana/Main2Activity.java
+++ app/src/main/java/com/example/zz7/hiragana/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.example.zz7.hiragana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent k = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(k);
+
+            }
+        });
+
     }
 }
